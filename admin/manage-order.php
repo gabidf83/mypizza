aae42952bd241911c7d4a9ac0310bf7adc640939
gabidf83 <gabrieldelgado83@hotmail.com>
2022-09-18 19:46:54 +0200

    delete admin

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,6 +4,47 @@
     <div class="wrapper">
         <h1>Gestión de pedidos</h1>  
 
+        <br/> <br/> <br/>
+
+        <table class="tbl-full">
+            <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Usuario</th>
+                <th>Acciones</th>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Gabriel Delgado</td>
+                <td>1234</td>
+                <td>
+                    <a href="#" class="btn-secondary">Actualizar</a>
+                    <a href="#" class="btn-danger">Borrar</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2</td>
+                <td>Admin</td>
+                <td>1234</td>
+                <td>
+                    <a href="#" class="btn-secondary">Actualizar</a>
+                    <a href="#" class="btn-danger">Borrar</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>Ana</td>
+                <td>1234</td>
+                <td>
+                    <a href="#" class="btn-secondary">Actualizar</a>
+                    <a href="#" class="btn-danger">Borrar</a>
+                </td>
+            </tr>
+        </table>
+
     </div>
 
     <div class="clearfix"></div>
