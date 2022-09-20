f7aaad63d6a7d7a5583fd62dab8cd862a1bb34da
gabidf83 <gabrieldelgado83@hotmail.com>
2022-09-20 07:57:28 +0200

    update admin

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,23 +4,83 @@
     <div class="wrapper">
         <h1>Gestión del administrador</h1>
 
+        <br/> <br/>
+
+        <?php
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add'];
+                unset($_SESSION['add']);
+            }
+
+            if(isset($_SESSION['delete']))
+            {
+                echo $_SESSION['delete'];
+                unset($_SESSION['delete']);
+            }
+
+            if(isset($_SESSION['update']))
+            {
+                echo $_SESSION['update'];
+                unset($_SESSION['update']);
+            }
+        ?>
+
+        <br/> <br/>
+
+        <a href="add-admin.php" class="btn-primary">Añadir administrador</a>
+
+        <br/> <br/> <br/>
+
         <table class="tbl-full">
             <tr>
                 <th>Id</th>
-                <th>Full name</th>
-                <th>Username</th>
-                <th>Actions</th>
+                <th>Nombre</th>
+                <th>Usuario</th>
+                <th>Acciones</th>
             </tr>
 
-            <tr>
-                <td>1.</td>
-                <td>Gabriel Delgado</td>
-                <td>1234</td>
-                <td>
-                    Update Admin
-                    Delete Admin
-                </td>
-            </tr>
+            <?php
+                //obtenemos los datos de la tabla tbl_admin
+                $sql = "SELECT * FROM tbl_admin";
+                $res = mysqli_query($conn, $sql) or die (mysqli_error());
+
+                if($res==TRUE)
+                {
+                    //si hay datos en la tabla obtenemos el numero de filas
+                    $count = mysqli_num_rows($res);
+                    
+                    if($count>0)
+                    {
+                        //con un while recorremos los resultados
+                        while($rows=mysqli_fetch_assoc($res))
+                        {
+                            $id = $rows['id'];
+                            $full_name = $rows['full_name'];
+                            $username = $rows['username'];
+
+            ?>
+
+                            <tr>
+                                <td><?php echo $id ?></td>
+                                <td><?php echo $full_name ?></td>
+                                <td><?php echo $username ?></td>
+                                <td>
+                                    <a href="<?php echo SITEURL;?>update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Actualizar</a>
+                                    <a href="<?php echo SITEURL;?>delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Borrar</a>
+                                </td>
+                            </tr>
+
+            <?php
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+            ?>
+
         </table>
 
     </div>
